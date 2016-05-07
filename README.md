89fe4e5e71685e5049202f72541da267e61ab0f3
Kei <kei@dji-qwy-mbp.local>
2016-05-07 20:41:38 +0800

    update 2016

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 Resume
 ===
 
-- [http://vinnpc.github.io/resume](http://vinnpc.github.io/resume)
-- [http://vinnpc.github.io/resume/zh-cn](http://vinnpc.github.io/resume/zh-cn)
-- [http://vinnpc.github.io/resume/zh-hk](http://vinnpc.github.io/resume/zh-hk)
+- [yqw1218.github.io/resume](http://yqw1218.github.io/resume)
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
-    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('clean', function () {
@@ -9,9 +9,10 @@ gulp.task('clean', function () {
 });
 
 gulp.task('sass', function () {
-  return sass('sass', {style: 'compact'})
-  .pipe(autoprefixer())
-  .pipe(gulp.dest('css'));
+  return gulp.src('sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function () {
